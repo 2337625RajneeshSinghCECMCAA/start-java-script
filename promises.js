96083ab6f69a9248771c4402e80321473a7fe9d6
Rajneesh123 <142533255+2337625RajneeshSinghCECMCAA@users.noreply.github.com>
2025-09-09 10:14:58 +0000

    promise update

---
+++ promises.js
@@ -0,0 +1,23 @@
+// const  promise = new Promise (function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("hii"); 
+//         resolve()   
+//     },2000)
+// }).then(function(){
+//     console.log("ram");
+    
+// })
+
+
+// const promise = new Promise(function(resolve,reeject){
+//     setTimeout(function(){
+//         resolve({username: "Rajneesh", email: "rajneesh@123.com"})
+//     },2000)
+// })
+
+// promise.then(function(user){
+//     console.log(user);
+    
+
+// })
+
