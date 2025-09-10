d1ed0eff36129f557d2d5e815c9a07407d6fed5e
Rajneesh123 <142533255+2337625RajneeshSinghCECMCAA@users.noreply.github.com>
2025-09-10 04:57:45 +0000

    promises updated

--- promises.js
+++ promises.js
@@ -21,3 +21,50 @@
 
 // })
 
+// const promisethree = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = true//false
+//         if(!error){
+//             resolve({username: "Rajneesh", email: "rajneesh@123.com"})
+//         }else{
+//             reject('ERROR: Something went wrong')
+//         }
+//     },2000)
+// })
+
+// promisethree
+// .then((user)=> {
+//     console.log(user);    
+// })
+// .catch((err)=>{
+//     console.log(err);    
+// })
+// .finally(()=>{console.log("final result")})
+
+
+
+// // async or wait
+
+// async function promisefour(){
+//     try {
+//         const response = await promisethree
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
+        
+//     }
+    
+// }
+// promisefour()
+
+
+fetch('http://jsonplaceholder.typicode.com/users')
+.then((response)=> {
+    return response.json()
+})
+.then((data)=> {
+    console.log(data);
+})
+.catch((error)=> {
+    console.log(error)
+})
